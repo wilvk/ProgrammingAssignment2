2de22fb8aeed3e37beaca294cc5f1a70771b159f
wilvk <willvk@gmail.com>
2015-10-24 18:43:43 +1100

    commit of R fil

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function makes the cache matrix and sets a boolean whether the 
+## matrix has changed or not to indicate to cacheSolve() whether 
+## the matrix needs to be solved or the cached version can be used.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix() ) {
 
+	m <- NULL
+	
+	matrixChanged <- TRUE
+	
+	if(exists("tempMatrix") && tempMatrix == x) {
+	    matrixChanged <- FALSE
+  	}
+  	else {
+    		tempMatrix <- x
+  	}
+		
+	set <- function (y) {
+		x <<- y
+		m <<- NULL
+	}
+
+	get <- function() x
+	
+	hasMatrixChanged <- function () matrixChanged
+
+	setMatrix <- function(solvedMatrix) m <<- solvedMatrix
+
+	getMatrix <- function() m
+
+	list(set = set, get = get,
+		setMatrix = setMatrix,
+		getMatrix = getMatrix,
+		hasMatrixChanged = hasMatrixChanged)
 }
 
 
-## Write a short comment describing this function
+## This function takes the list from makeCacheMatrix and returns the value
+## of the solved matrix. If the matrix has been previously solved, the existing
+## solved matrix value is used, otherwise the matrix is solved.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  m <- x$getMatrix()
+  
+	if(!is.null(m) && x$hasMatrixChanged() ) {
+		message("getting cached matrix")
+		return(m)
+	}
+
+	data <- x$get()
+
+	m <- solve(data, ...)
+
+	x$setMatrix(m)
+
+	m
 }
